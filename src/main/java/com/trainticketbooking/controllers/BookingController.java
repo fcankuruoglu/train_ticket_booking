9ad86ea57872e6dfeee6bb6f7d84269e4dce338b
Ferruhcan Kuruoğlu <ferruhcankuruoglu@gmail.com>
2022-10-01 14:46:07 +0300

    Fixing error and setting for deploying.

--- src/main/java/com/trainticketbooking/controllers/BookingController.java
+++ src/main/java/com/trainticketbooking/controllers/BookingController.java
@@ -3,13 +3,14 @@ package com.trainticketbooking.controllers;
 import com.trainticketbooking.dtos.RequestDTO;
 import com.trainticketbooking.dtos.ResponseDTO;
 import com.trainticketbooking.services.BookingService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/v1/book")
 public class BookingController {
 
     private final BookingService bookingService;
@@ -18,8 +19,8 @@ public class BookingController {
         this.bookingService = bookingService;
     }
 
-    @PostMapping("/bookTrainTicket")
-    ResponseDTO bookTrainTicket(@RequestBody RequestDTO request){
-        return bookingService.booking(request);
+    @PostMapping("/ticket")
+    ResponseEntity<ResponseDTO> bookTrainTicket(@RequestBody RequestDTO request){
+        return ResponseEntity.ok().body(bookingService.booking(request));
     }
 }
--- src/main/java/com/trainticketbooking/services/BookingServiceImpl.java
+++ src/main/java/com/trainticketbooking/services/BookingServiceImpl.java
@@ -1,5 +1,6 @@
 package com.trainticketbooking.services;
 
+import ch.qos.logback.core.Layout;
 import com.trainticketbooking.dtos.CarriageDTO;
 import com.trainticketbooking.dtos.LayoutDetailDTO;
 import com.trainticketbooking.dtos.RequestDTO;
@@ -7,40 +8,78 @@ import com.trainticketbooking.dtos.ResponseDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @Service
 public class BookingServiceImpl implements BookingService{
+    private final float MAX_OCCUPANCY_RATIO =(float) 0.7;
     @Override
     public ResponseDTO booking(RequestDTO request) {
         ResponseDTO response = new ResponseDTO();
         ArrayList<CarriageDTO> availableCarriages =  findUnder70Carriages(request.getTrain().getCarriages());
-        //ArrayList<LayoutDetailDTO> layoutDetails = placedPassengers(availableCarriages, request.getNumberOfPassenger());
 
-        // if there is no available carriage, it returns with false and empty array.
-        if (availableCarriages.size() == 0) {
+        if (availableCarriages.size() == 0 || !checkCapacity(availableCarriages, request.getCanBePlaceInDifferentCarriages() ,request.getNumberOfPassenger())) {
             response.setLayoutDetails(null);
             response.setCanBeBooked(false);
             return response;
         }
+        if (!request.getCanBePlaceInDifferentCarriages()){
+            response.setLayoutDetails(placedPassengersOnSameCarriage(availableCarriages, request.getNumberOfPassenger()));
+        }else {
+            response.setLayoutDetails(placedPassengers(availableCarriages, request.getNumberOfPassenger()));
+        }
         response.setCanBeBooked(true);
-        response.setLayoutDetails(placedPassengers(availableCarriages, request.getNumberOfPassenger()));
-
-
         return response;
     }
-    // Find all carriages with an occupancy ratio less than %70.
     private ArrayList<CarriageDTO> findUnder70Carriages(CarriageDTO[] carriages){
         ArrayList<CarriageDTO> under70Carriages= new ArrayList<>();
         for (int i = 0; i < carriages.length; i++){
             float occupancyRatio = (float) carriages[i].getOccupiedSeat() / carriages[i].getCapacity();
-            if (occupancyRatio < 0.7){
+            if (occupancyRatio < MAX_OCCUPANCY_RATIO){
                 under70Carriages.add(carriages[i]);
             }
         }
         return under70Carriages;
     }
-    // Placed all passengers in available carriages.
+    private boolean checkCapacity(ArrayList<CarriageDTO> carriages, boolean canBePlaceInDifferentCarriages, int numberOfPassenger){
+        int capacity = 0;
+        if (canBePlaceInDifferentCarriages){
+            for (CarriageDTO carriage :
+                    carriages) {
+                int availableSeats = ((int)(carriage.getCapacity() * MAX_OCCUPANCY_RATIO) -  carriage.getOccupiedSeat());
+                if (capacity < availableSeats){
+                    capacity = availableSeats;
+                }
+            }
+        }else {
+            for (CarriageDTO carriage :
+                    carriages) {
+                capacity = capacity + ((int)(carriage.getCapacity() * MAX_OCCUPANCY_RATIO) -  carriage.getOccupiedSeat());
+            }
+        }
+        if (capacity < numberOfPassenger){
+            return false;
+        }
+        return true;
+    }
+    private List<LayoutDetailDTO> placedPassengersOnSameCarriage(ArrayList<CarriageDTO> availableCarriages, int numberOfPassenger){
+        ResponseDTO response = new ResponseDTO();
+        Random random = new Random();
+        ArrayList<LayoutDetailDTO> layoutDetails = new ArrayList<>();
+
+        while(true){
+            int randomIndex = random.nextInt(availableCarriages.size());
+            float newOccupancyRatio =(float)(availableCarriages.get(randomIndex).getOccupiedSeat() + numberOfPassenger) / availableCarriages.get(randomIndex).getCapacity();
+            if(newOccupancyRatio < 0.7) {
+                LayoutDetailDTO tmp = new LayoutDetailDTO();
+                tmp.setCarriageName(availableCarriages.get(randomIndex).getName());
+                tmp.setNumberOfPassenger(numberOfPassenger);
+                layoutDetails.add(tmp);
+                break;
+            }
+        }
+
+        return layoutDetails;
+    }
     private ArrayList<LayoutDetailDTO> placedPassengers(ArrayList<CarriageDTO> availableCarriages, int numberOfPassenger){
         Random random = new Random();
         ArrayList<LayoutDetailDTO> layoutDetails = new ArrayList<>();
@@ -49,24 +88,33 @@ public class BookingServiceImpl implements BookingService{
             LayoutDetailDTO tmp = new LayoutDetailDTO();
             int randomPassengers = random.nextInt(numberOfPassenger + 1 - 1 ) + 1;
             int randomIndex = random.nextInt(availableCarriages.size());
+            float newOccupancyRatio =(float)(availableCarriages.get(randomIndex).getOccupiedSeat() + randomPassengers) / availableCarriages.get(randomIndex).getCapacity();
+            System.out.println("ratio: " +newOccupancyRatio);
+            System.out.println("randomPassengers: " +randomPassengers);
+
             tmp.setNumberOfPassenger(randomPassengers);
             tmp.setCarriageName(availableCarriages.get(randomIndex).getName());
-
-            if (layoutDetails.size() == 0) {
-                layoutDetails.add(tmp);
-            }else {
-                boolean stateCheck = false;
-                for(int i=0; i<layoutDetails.size();i++){
-                    if(tmp.getCarriageName() == layoutDetails.get(i).getCarriageName()){
-                        System.out.println("first: "+tmp.getCarriageName() + " --- "+layoutDetails.get(i));
-                        layoutDetails.get(i).setNumberOfPassenger(layoutDetails.get(i).getNumberOfPassenger() + randomPassengers);
-                        stateCheck = true;
+            if(newOccupancyRatio < 0.7) {
+                if (layoutDetails.size() == 0) {
+                    layoutDetails.add(tmp);
+                    availableCarriages.get(randomIndex).setOccupiedSeat(availableCarriages.get(randomIndex).getOccupiedSeat() + randomPassengers);
+                }else {
+                    boolean stateCheck = false;
+                    for(int i=0; i<layoutDetails.size();i++){
+                        if(tmp.getCarriageName() == layoutDetails.get(i).getCarriageName()){
+                            layoutDetails.get(i).setNumberOfPassenger(layoutDetails.get(i).getNumberOfPassenger() + randomPassengers);
+                            availableCarriages.get(randomIndex).setOccupiedSeat(availableCarriages.get(randomIndex).getOccupiedSeat() + randomPassengers);
+                            stateCheck = true;
+                        }
                     }
+                    if (stateCheck == false) {
+                        layoutDetails.add(tmp);
+                        availableCarriages.get(randomIndex).setOccupiedSeat(availableCarriages.get(randomIndex).getOccupiedSeat() + randomPassengers);
+                    };
                 }
-                if (stateCheck == false) {layoutDetails.add(tmp);};
+                numberOfPassenger = numberOfPassenger - randomPassengers;
             }
-            numberOfPassenger = numberOfPassenger - randomPassengers;
-        }
+            }
 
         sortLayoutDetail(layoutDetails);
         return layoutDetails;
